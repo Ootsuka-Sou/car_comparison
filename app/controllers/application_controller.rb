913dc6c2fce6e1f1c89410650fb84b2583f4b88e
Ootsuka-Sou <hungerheart0130@outlook.jp>
2021-06-28 14:25:05 +0000

    add name profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+    
+    protect_from_forgery with: :exception
+    
+    #ログイン済みのユーザーのみにアクセスを許可する
+    before_action :authenticate_user!
+    
+    #deviseコントローラーにストロングパラメータを追加する
+    #ストロングパラメーターとはコントローラで送られてきたパラメータを安全に受け取る為の仕組み
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    
+    #ストロングパラメーターの初期設定の段階では、メールアドレスとパスワードのみ
+    #新しいキーを受け取らせる為には編集が必要
+    protected
+    def configure_permitted_parameters
+        #devise_parameter_samotizer.permit(追加したいメソッドの種類, keys, [追加したいパラメーター名])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+    end
+    
 end

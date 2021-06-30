c5b3ef4e8bd256e36d81cc45471b277a0d51ddc8
Ootsuka-Sou <hungerheart0130@outlook.jp>
2021-06-30 07:21:50 +0000

    add_mypage

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  #before_actionはRuby on Railsのコールバックと呼ばれるメソッド
+  #コールバックは特定のタイミングで呼び出されるメソッド
+  #before_actionはアクションが呼び出される前に呼び出される
+  before_action : set_user
+  
+  def edit
+    
+  end
+
+  def update
+    #update_without_passwordメソッドはパスワード無しでユーザー情報を更新できるメソッド
+    @user.update_without_password(user_params)
+    redirect_to mypage_users_url
+  end
+
+  def mypage
+    
+  end
+  
+  private
+  
+  def set_user
+    @user = current_user
+  end
+  
+  def user_params
+    params.permit(:email, :encrypted_password, :reset_password_token, :name)
+  
+end

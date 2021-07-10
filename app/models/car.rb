ff8fb5240a888ced8207a38acab6fe1ac42df883
Ootsuka-Sou <hungerheart0130@outlook.jp>
2021-07-10 13:28:45 +0000

    add grade

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,27 @@ class Car < ApplicationRecord
     validates :name, presence: true
     validates :money, presence: true
     validates :color, presence: true
+    
+    enum name: {
+        spacia: 0,
+        n_box: 1,
+        tanto: 2
+    },_prefix: true
+    
+    enum color: {
+    white: 0,
+    black: 1,
+    shilver: 2,
+    red: 3,
+    blue: 4,
+    green: 5,
+    ivory: 6,
+    pink: 7,
+    brown: 8,
+    skyblue: 9,
+    opwhite: 10,
+    twotone: 11,
+    optwotone: 12
+  }, _prefix: true
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :grades
   resources :cars
   devise_for :users, controllers: { registrations: 'registrations' }
 get "users/show" => "users#show"
